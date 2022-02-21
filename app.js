bd83de3b1e26e6410934cd3e5b27b7f519d9ae67
vimalrajan242001 <vimalrajansret@gmail.com>
2022-02-21 23:57:38 +0530

    das

--- app.js
+++ app.js
@@ -108,6 +108,13 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
     res.send("sda");
 });
+if (process.env.NODE_ENV === "production") {
+    //set static folder
+    app.use(express.static("client/build"));
+}
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 app.use("/api/supplier", supplierRoutes);
 app.use("/api/inventory", inventoryRoutes);
 app.use("/api/user", userRoutes);
